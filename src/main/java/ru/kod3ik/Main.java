92996d966284407f8e54246fdca6154360e3b81a
Zalina Kodz <kod3ik87@gmail.com>
2023-02-15 18:46:54 +0300

    hello massage was changed

--- src/main/java/ru/kod3ik/Main.java
+++ src/main/java/ru/kod3ik/Main.java
@@ -2,6 +2,7 @@ package ru.kod3ik;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Well hello!");
     }
 }
